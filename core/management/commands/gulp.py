1a8c2fdc66ceef0ae56a6d684591eb4d17ca1fb1
Kayode Adechinan <kayode.adechinan@gmail.com>
2023-04-02 02:47:09 +0000

    Update gulp.py

--- core/management/commands/gulp.py
+++ core/management/commands/gulp.py
@@ -10,7 +10,7 @@ def prod():
         os.system("cd core/gulp &&  npm install")
     if os.path.isdir(staticfiles):
         shutil.rmtree(staticfiles)
-    os.system("cd core/gulp &&  gulp")
+    os.system("cd core/gulp &&  npx gulp")
     os.system("python manage.py  collectstatic --noinput --no-post-process")
     os.system('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
 
